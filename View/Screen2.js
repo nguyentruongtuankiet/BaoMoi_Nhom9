70e1bebc709fad6f7328aeb748a5c07f59db03a2
phunguyen <98088905+phunguyenst@users.noreply.github.com>
2023-10-30 15:18:31 +0700

    add file

---
+++ View/Screen2.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Screen2 = () => {
+  return (
+    <View>
+      <Text>Screen2</Text>
+      <Text>VanPhu</Text>
+    </View>
+  )
+}
+
+export default Screen2
