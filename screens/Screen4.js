f0e2898cf94b5b96a99995e8355044e85af66b23
TuanKiet <nguyentruongtuankiet2002@gmail.com>
2023-11-25 21:23:44 +0700

    update video

--- screens/Screen4.js
+++ screens/Screen4.js
@@ -1,87 +1,175 @@
+import React, { useRef, useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
   Text,
   Image,
-  TextInput,
   TouchableOpacity,
   ScrollView,
-  FlatList
+  FlatList,
+  Dimensions,
 } from "react-native";
-import React from "react";
+import { Video } from "expo-av";
+import { AntDesign } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
 const data = [
   {
-    link: "https://inkythuatso.com/uploads/thumbnails/800/2023/02/tao-dang-chup-anh-di-dao-trong-rung-3-22-09-46-55.jpg",
-    name: "Chụp ảnh chiều thu trên đồi đà lạt thơ mộng ",
+    link: "video.mp4",
+    name: "Mỹ, Trung Quốc tranh vai trò hòa giải ở Gaza, bên nào “thắng”?",
   },
   {
-    link: "https://nghethuat.vn/wp-content/uploads/2022/11/pasted-image-0-1.png",
-    name: "Cặp đôi từng hứa yêu nhau trọn đời đã chia tay sau khi đi chơi đà lạt",
+    link: "video2.mp4",
+    name: "Thanh niên Việt Nam 'LẦN ĐẦU' gặp Ronaldo ngoài đời và 'BIỂU CẢM' cực hài",
+  },
+  {
+    link: "video3.mp4",
+    name: "CÚ HATTRICK HỦY DIỆT CỦA MESSI - MBAPPE - NEYMAR",
+  },
+  {
+    link: "video4.mp4",
+    name: "Mpabee toả sáng như Beckharm",
   },
 ];
 
 export default function Screen4() {
+  const [videoRefs, setVideoRefs] = useState({});
+  const [isPlaying, setIsPlaying] = useState({});
+
+  useEffect(() => {
+    const refs = {};
+    const playingState = {};
+    data.forEach((item) => {
+      refs[item.link] = React.createRef();
+      playingState[item.link] = false;
+    });
+    setVideoRefs(refs);
+    setIsPlaying(playingState);
+  }, []);
+
+  const playPauseVideo = async (link) => {
+    if (videoRefs[link].current) {
+      const status = await videoRefs[link].current.getStatusAsync();
+      if (status.isPlaying) {
+        videoRefs[link].current.pauseAsync();
+      } else {
+        videoRefs[link].current.playAsync();
+      }
+      setIsPlaying({
+        ...isPlaying,
+        [link]: !isPlaying[link],
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
-    <View style = {{flex: 1, backgroundImage: "linear-gradient(to right, #5eb4ba, #155380)", alignItems: "center"}}>
-        <Text style = {styles.fontVideo}>Video</Text>
-    </View>
-    <View style = {styles.listvideo}>
+      <View style={styles.header}>
+        <LinearGradient
+          colors={["#5eb4ba", "#155380"]}
+          style={styles.headerGradient}
+        >
+          <Text style={styles.fontVideo}>Video</Text>
+        </LinearGradient>
+      </View>
 
-    <ScrollView
-      nestedScrollEnabled={true}
-    >
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={{ height: 300, width: 330, backgroundColor:"#FFF",marginTop:5 }}>
-            <Image
-              style={{ height: 170, width: 330, borderRadius: 21  }}
-              source={{ uri: item.link }}
-            />
-            <Image
-              style={{ height: 30, width: 100, marginTop:5 }}
-              source={require("../images/logoKenh.png")}
-              resizeMode="contain"
-            />
-            <Text style={{width:330,color:"#000",fontFamily:"Inter",fontSize:19,fontStyle:"normal",fontWeight:600}}>{item.name}</Text>
-          </View>
-        )}
-      />
-    </ScrollView>
-    </View>
-   
-      
+      <View style={styles.listvideo}>
+        <ScrollView nestedScrollEnabled={true}>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.link}
+            renderItem={({ item }) => (
+              <View style={styles.videoContainer}>
+                <Video
+                  ref={videoRefs[item.link]}
+                  source={require(`../video/${item.link}`)}
+                  style={styles.video}
+                  resizeMode="contain"
+                  shouldPlay={false}
+                  isLooping={false}
+                  useNativeControls
+                />
+                <TouchableOpacity
+                  onPress={() => playPauseVideo(item.link)}
+                  style={styles.playPauseButton}
+                >
+                  <AntDesign
+                    name={isPlaying[item.link] ? "pausecircleo" : "playcircleo"}
+                    size={50}
+                    color="#fff"
+                  />
+                </TouchableOpacity>
+                <Image
+                  style={styles.logo}
+                  source={require("../images/logoKenh.png")}
+                  resizeMode="cover"
+                />
+                <Text style={styles.videoName}>{item.name}</Text>
+              </View>
+            )}
+          />
+        </ScrollView>
+      </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  listvideo:{
-    flex: 9,
+  headerGradient: {
+    flex: 1,
+    width: "100%",
     alignItems: "center",
-
+    justifyContent: "center",
   },
-  fontVideo:{
-    textAlign: "center",
+  header: {
+    height: screenHeight * 0.1,
+    backgroundImage: "linear-gradient(to right, #5eb4ba, #155380)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listvideo: {
+    flex: 1,
+  },
+  fontVideo: {
     fontSize: 20,
     fontWeight: "bold",
     color: "#fff",
-    marginTop:15
-  }
-});
+  },
+  videoContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
 
-{
-  /* <Video
-        source={{
-          uri: "https://youtu.be/-5q5mZbe3V8?list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbAVM-5q5mZbe3V8",
-        }} // Thay đổi URL video tại đây
-        style={styles.video}
-        controls={true} // Hiển thị nút điều khiển
-        resizeMode="contain" // Tùy chỉnh chế độ hiển thị video
-      /> */
-}
+    marginBottom: 50,
+  },
+  video: {
+    width: screenWidth,
+    height: screenHeight * 0.4,
+    backgroundColor: "transparent",
+  },
+  playPauseButton: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    zIndex: 1,
+    transform: [{ translateX: -25 }, { translateY: -25 }],
+  },
+  logo: {
+    height: 30,
+    width: 100,
+    marginTop: 5,
+  },
+  videoName: {
+    width: "80%",
+    color: "#000",
+    fontFamily: "Inter",
+    fontSize: 19,
+    fontStyle: "normal",
+    fontWeight: 600,
+  },
+});
